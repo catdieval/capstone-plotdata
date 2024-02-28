11143630101c27119c9c9a803b273cd8f7af3472
Maria Adner <juergens.maria@googlemail.com>
2024-02-28 12:33:17 +0100

    Created Layout component

--- Components/Layout/index.js
+++ Components/Layout/index.js
@@ -1,3 +1,14 @@
-export default function Layout() {
-  return <div></div>;
+import Title from "../Title";
+import Head from "next/head";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Head>
+        <title>PlotData</title>
+      </Head>
+      <main>{children}</main>
+      <Title />
+    </div>
+  );
 }
