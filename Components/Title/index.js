ed2a9eb5a746b51aaf531efb851e84e72301b30a
YavorLalev <yavorlalev@abv.bg>
2024-02-28 10:50:50 +0100

    added--Title-component

---
+++ Components/Title/index.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import Image from "next/image";
+import logoImage from "../../assets/plotdata-logo.png";
+
+const StyledTitle = styled.section`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: white;
+  margin: 0;
+  padding: 10px;
+`;
+
+export default function Title() {
+  return (
+    <>
+      <StyledTitle>
+        <Image src={logoImage} alt={""} width={300} height={74} />
+      </StyledTitle>
+    </>
+  );
+}
