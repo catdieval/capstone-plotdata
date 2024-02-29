84f4696dbf14a0d270ac76de50309bc31ae91c4a
YavorLalev <yavorlalev@abv.bg>
2024-02-29 10:33:01 +0100

    declare-hiddenFileInput-variable

--- Components/FileUploader/index.js
+++ Components/FileUploader/index.js
@@ -1,9 +1,12 @@
 import Button from "@/Layout/Button";
+import { useRef } from "react";
 
 export default function FileUploader() {
+  const hiddenFileInput = useRef(null);
   return (
     <>
       <Button onClick={""}>Choose a file</Button>
+      <input type="file" accept=".csv" ref={hiddenFileInput} />
     </>
   );
 }
