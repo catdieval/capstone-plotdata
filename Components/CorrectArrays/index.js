451bc024517464da20aa141f292f04a5619e7c94
Maria Adner <juergens.maria@googlemail.com>
2024-02-29 10:49:44 +0100

    CorrectArray function added

---
+++ Components/CorrectArrays/index.js
@@ -0,0 +1,16 @@
+export default function CorrectArrays(arrayofobjects) {
+  const [keyitems, ...csvArray] = arrayofobjects;
+  const csvArray2 = csvArray.filter(
+    (row, index, arr) => index !== arr.length - 1
+  );
+
+  let csvArray3 = [...csvArray2];
+  for (let i = 0; i < csvArray3.length; i++) {
+    for (key in csvArray3[i]) {
+      if (csvArray3[i][key] == "null") {
+        csvArray3[i][key] = JSON.parse(csvArray3[i][key]);
+      }
+    }
+  }
+  return [keyitems, ...csvArray3];
+}
