e4a92dbecdb5b2b74695d55ad7b36348491170a1
YavorLalev <yavorlalev@abv.bg>
2024-02-29 10:58:50 +0100

    create-handleChange-function

--- Components/FileUploader/index.js
+++ Components/FileUploader/index.js
@@ -1,17 +1,27 @@
 import Button from "@/Layout/Button";
 import { useRef } from "react";
 
-export default function FileUploader() {
+export default function FileUploader({ handleFile }) {
   const hiddenFileInput = useRef(null);
 
   function handleClick() {
     hiddenFileInput.current.click();
   }
 
+  function handleChange(event) {
+    const fileUploaded = event.target.files[0];
+    handleFile(fileUploaded);
+  }
+
   return (
     <>
       <Button onClick={handleClick}>Choose a file</Button>
-      <input type="file" accept=".csv" ref={hiddenFileInput} />
+      <input
+        type="file"
+        accept=".csv"
+        ref={hiddenFileInput}
+        onChange={handleChange}
+      />
     </>
   );
 }
