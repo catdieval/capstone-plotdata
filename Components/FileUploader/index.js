0622fbc2ba6d3f5b8306166144366b5bef595b79
YavorLalev <yavorlalev@abv.bg>
2024-02-29 10:36:04 +0100

    added-handleClick-function

--- Components/FileUploader/index.js
+++ Components/FileUploader/index.js
@@ -3,9 +3,14 @@ import { useRef } from "react";
 
 export default function FileUploader() {
   const hiddenFileInput = useRef(null);
+
+  function handleClick() {
+    hiddenFileInput.current.click();
+  }
+
   return (
     <>
-      <Button onClick={""}>Choose a file</Button>
+      <Button onClick={handleClick}>Choose a file</Button>
       <input type="file" accept=".csv" ref={hiddenFileInput} />
     </>
   );
