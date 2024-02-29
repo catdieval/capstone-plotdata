e5c2c2470bdea470ea2b05a1514b06eedb3424a3
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-02-29 16:43:00 +0100

    Added back text-align set to center for Heading

--- Layout/Heading/index.js
+++ Layout/Heading/index.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
-const StyledHeading = styled.h2``;
+const StyledHeading = styled.h2`
+  text-align: center;
+`;
 
 export default function Heading({ children }) {
   return (
