bf66b081814a1a28c9ed11e36835a950c7f35634
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-02-29 10:37:56 +0100

    Added some imports in UploadData

---
+++ Components/UploadData/index.js
@@ -0,0 +1,10 @@
+import Heading from "../../Layout/Heading";
+import Paragraph from "../../Layout/Paragraph";
+import InputTypeSubmit from "../../Layout/InputTypeSubmit";
+import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
+const { convertCSVToArray } = require("convert-csv-to-array");
+
+export default function UploadData() {
+  return <></>;
+}
