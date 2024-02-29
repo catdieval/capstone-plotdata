7861df64289794f7c3dc7a5ae77edee74ec8bbd1
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-02-29 11:37:23 +0100

    Added states to UploadData

--- Components/UploadData/index.js
+++ Components/UploadData/index.js
@@ -1,10 +1,25 @@
 import Heading from "../../Layout/Heading";
 import Paragraph from "../../Layout/Paragraph";
 import InputTypeSubmit from "../../Layout/InputTypeSubmit";
+import CorrectArrays from "../CorrectArrays";
+import FileUploader from "../FileUploader";
 import { useState } from "react";
 import useLocalStorageState from "use-local-storage-state";
 const { convertCSVToArray } = require("convert-csv-to-array");
 
 export default function UploadData() {
+  //State to store keys from the CSV file
+  const [keynames, setKeyNames] = useLocalStorageState("keynames", {
+    defaultValue: [],
+  });
+
+  //State to store the values
+  const [vals, setVals] = useLocalStorageState("vals", {
+    defaultValue: [],
+  });
+
+  //State to store the file object
+  const [fileObj, setFileObj] = useState({});
+
   return <></>;
 }
