a20dd3ebc3c019be1207de3ae5d06acaf7cf8b4a
YavorLalev <yavorlalev@abv.bg>
2024-02-29 09:45:47 +0100

    fix-paragraph-component

--- Components/Paragraph/index.js
+++ Components/Paragraph/index.js
@@ -4,6 +4,6 @@ const StyledP = styled.p`
   text-align: center;
 `;
 
-export default function Paragraph() {
-  return <StyledP></StyledP>;
+export default function Paragraph({ children }) {
+  return <StyledP>{children}</StyledP>;
 }
