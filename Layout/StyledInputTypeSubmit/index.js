6ac848cd20d016655a5518c20b2d208904e53d3f
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-02-29 17:40:41 +0100

    Added StyledInputTypeSubmit component

---
+++ Layout/StyledInputTypeSubmit/index.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledInputTypeSubmit = styled.input.attrs({
+  type: "submit",
+})`
+  background-color: deepskyblue;
+  padding: 10px;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+`;
