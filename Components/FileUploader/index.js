cae1ad82c940c135b43c5d43bb9cee15f3c393cf
YavorLalev <yavorlalev@abv.bg>
2024-03-04 11:59:52 +0100

    added-comments-fileUploader

--- Components/FileUploader/index.js
+++ Components/FileUploader/index.js
@@ -6,12 +6,16 @@ const StyledInput = styled.input`
   display: none;
 `;
 export default function FileUploader({ onFileUploaded }) {
+  // Create a reference to the hidden file input element
   const hiddenFileInput = useRef(null);
 
+  // Programatically click the hidden file input element
+  // when the Button component is clicked
   function handleClick() {
     hiddenFileInput.current.click();
   }
-
+  // Call a function (passed as a prop from the parent component)
+  // to handle the user-selected file
   function handleChange(event) {
     const fileUploaded = event.target.files[0];
     onFileUploaded(fileUploaded);
