52a693f00a790ae2829b001b0849361c63fbc18c
YavorLalev <yavorlalev@abv.bg>
2024-03-04 11:59:20 +0100

    added-comments

--- Components/CorrectArrays/index.js
+++ Components/CorrectArrays/index.js
@@ -1,5 +1,13 @@
 export default function CorrectArrays(arrayOfObjects) {
+  // The first row in arraysOfObjects is an array of key names,
+  // and the remaining rows are an arrays of objects
+
+  // The array of objects returned by convertCSVToArray has one row
+  // too many at the end (a copy of the last row), so we remove it
   const [keyItems, ...csvArray] = arrayOfObjects.slice(0, -1);
+
+  // The array of objects returned by convertCSVToArray converts the
+  // null values into strings, but we want to undo this.
 
   const correctedArray = csvArray.map((row) =>
     Object.fromEntries(
