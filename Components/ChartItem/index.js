31369b2a7439122e645d388f52bad875829fd015
Maria Adner <juergens.maria@googlemail.com>
2024-03-06 11:00:27 +0100

    Fixed ChartItem

--- Components/ChartItem/index.js
+++ Components/ChartItem/index.js
@@ -1,29 +1,27 @@
 import useLocalStorageState from "use-local-storage-state";
-import FlexDiv from "../FlexContainer";
 import Button from "../Button";
 import Image from "next/image";
-import { chartarray } from "@/lib/listofcharttypes";
-import barplot from "../../assets/chart-types/bar-plot.svg";
-import BarPlot from "@/utils/barplot";
 import { FlexContainer } from "../FlexContainer";
 
-const name = "bar-plot";
-
-export default function ChartItem() {
+export default function ChartItem({ name }) {
   const [clickedChartType, setClickedChartType] = useLocalStorageState(
     "clickedChartType",
     { defaultValue: "" }
   );
 
   function handleClick(event) {
-    setclickedChartType(event.target.innerText);
+    setClickedChartType(event.target.innerText);
   }
 
   return (
     <FlexContainer $direction="column">
-      <Button onClick={handleClick}>{`${name}`}</Button>
-      <BarPlot />
-      {/* <Image src={barplot} height={200} width={200} alt={chartarray.name} /> */}
+      <Button onClick={handleClick}>{name}</Button>
+      <Image
+        src={`/../../assets/chart-types-png/${name}`}
+        height={200}
+        width={200}
+        alt={name}
+      />
     </FlexContainer>
   );
 }
