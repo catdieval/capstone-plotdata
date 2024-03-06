d42e9b4cdfe70accc7d3cad220c2250854efb474
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-03-06 12:48:11 +0100

    Removed commented out code

--- Components/FlexContainer/index.js
+++ Components/FlexContainer/index.js
@@ -1,16 +1,5 @@
 import styled from "styled-components";
 
-// const S = styled.div`
-//   flex-direction: ${(props) => (props.$direction ? "column" : "row")};
-//   display: flex;
-//   //flex-wrap: wrap;
-//   justify-content: space-around;
-// `;
-
-// export default function FlexDiv({ children, $direction }) {
-//   return <StyledDiv $direction={$direction}>{children}</StyledDiv>;
-// }
-
 export const FlexContainer = styled.section`
   flex-direction: ${(props) =>
     props.$direction === "column" ? "column" : "row"};
