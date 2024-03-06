e8b86f912733f9e97538676c325621b45d99b469
YavorLalev <yavorlalev@abv.bg>
2024-03-06 12:40:18 +0100

    fixed-listOfCharts-component

--- Components/ListOfCharts/index.js
+++ Components/ListOfCharts/index.js
@@ -1,14 +1,14 @@
 import Heading from "../Heading";
 import { FlexContainer } from "../FlexContainer";
 import ChartItem from "../ChartItem";
-import { chartarray } from "@/lib/listofcharttypes";
+import { chartArray } from "@/lib/listOfPlotTypes";
 
 export default function ListOfCharts() {
   return (
     <>
       <Heading>Step 2: Select the type of chart</Heading>
       <FlexContainer $direction="row">
-        {chartarray.map((chart) => {
+        {chartArray.map((chart) => {
           return (
             <ChartItem key={chart.name} icon={chart.icon} name={chart.name} />
           );
