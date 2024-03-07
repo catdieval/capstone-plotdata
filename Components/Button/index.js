788d9655604c731a16845ee49cc1e60b469f4b25
YavorLalev <yavorlalev@abv.bg>
2024-03-07 10:17:35 +0100

    changed-styling-of-button

--- Components/Button/index.js
+++ Components/Button/index.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 const StyledButton = styled.button`
   background-color: #1f77b4;
+  border-radius: 12px;
+  border: none;
   padding: 10px;
   cursor: pointer;
   font-size: 16px;
