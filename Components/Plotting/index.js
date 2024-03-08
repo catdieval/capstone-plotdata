f938c251441f95e056ed819ec41b187c0950aa9d
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-03-08 12:35:49 +0100

    Updated Plotting component

--- Components/Plotting/index.js
+++ Components/Plotting/index.js
@@ -4,24 +4,12 @@ import { chartArray } from "../../lib/listOfPlotTypes";
 import dynamic from "next/dynamic";
 const Plot = dynamic(() => import("react-plotly.js"), { ssr: false });
 
-export default function Plotting() {
-  const [clickedChartType, setClickedChartType] = useState("");
-  useEffect(() => {
-    const clickedChartTypeData = JSON.parse(
-      localStorage.getItem("clickedChartType")
-    );
-    if (clickedChartType) {
-      setClickedChartType(clickedChartType);
-    }
-    console.log("Test", clickedChartTypeData);
-  }, []);
-
-  // const clickedChartTypeData = localStorage.getItem("clickedChartType");
-  const clickedChartTypeData = "bar-plot";
+export default function Plotting({ clickedChartType }) {
+  console.log(`At the end we have ${clickedChartType}`);
   const XVariable = [1, 2, 3, 4, 6, 8, 10, 12, 14, 16, 18, 20];
   const YVariable = [32, 37, 40.5, 43, 49, 54, 59, 63.5, 69.5, 73, 74, null];
   const match = chartArray.findIndex(
-    (chart) => chart.name === clickedChartTypeData
+    (chart) => chart.name === clickedChartType
   );
 
   if (match != -1) {
