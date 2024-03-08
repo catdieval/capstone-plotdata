5e4a5e3fee18121047d701ba0c1a0ed4f12ed12d
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-03-08 12:37:59 +0100

    Updated ListOfCharts component

--- Components/ListOfCharts/index.js
+++ Components/ListOfCharts/index.js
@@ -3,14 +3,19 @@ import { FlexContainer } from "../FlexContainer";
 import ChartItem from "../ChartItem";
 import { chartArray } from "../../lib/listOfPlotTypes";
 
-export default function ListOfCharts() {
+export default function ListOfCharts({ handleChartType }) {
   return (
     <>
       <Heading>Step 2: Select the type of chart</Heading>
       <FlexContainer $grid="grid">
         {chartArray.map((chart) => {
           return (
-            <ChartItem key={chart.name} icon={chart.icon} name={chart.name} />
+            <ChartItem
+              key={chart.name}
+              icon={chart.icon}
+              name={chart.name}
+              handleChartType={handleChartType}
+            />
           );
         })}
       </FlexContainer>
