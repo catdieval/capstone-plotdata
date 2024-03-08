2b9bb443ac39a20302484c248033ab3447cb3a34
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-03-08 12:37:22 +0100

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import UploadData from "../Components/UploadData";
 import ListOfCharts from "../Components/ListOfCharts";
-import Plotting from "@/Components/Plotting";
+import Plotting from "../Components/Plotting";
 
-export default function HomePage() {
+export default function HomePage({ clickedChartType, handleChartType }) {
   return (
     <div>
       <UploadData />
-      <ListOfCharts />
-      <Plotting />
+      <ListOfCharts handleChartType={handleChartType} />
+      <Plotting clickedChartType={clickedChartType} />
     </div>
   );
 }
