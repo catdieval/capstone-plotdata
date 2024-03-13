303678b508096cf01f2997334c2c0d09dcc86b7c
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-03-13 13:57:07 +0100

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,22 @@ import UploadData from "../Components/UploadData";
 import ListOfCharts from "../Components/ListOfCharts";
 import Plotting from "../Components/Plotting";
 
-export default function HomePage({ clickedChartType, onSelectChartType }) {
+export default function HomePage({
+  fileObj,
+  isUploaded,
+  onUploadFile,
+  onConversion,
+  clickedChartType,
+  onSelectChartType,
+}) {
   return (
     <div>
-      <UploadData />
+      <UploadData
+        fileObj={fileObj}
+        isUploaded={isUploaded}
+        onUploadFile={onUploadFile}
+        onConversion={onConversion}
+      />
       <ListOfCharts onSelectChartType={onSelectChartType} />
       <Plotting clickedChartType={clickedChartType} />
     </div>
