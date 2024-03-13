e40097637c56187b0115c6a0dab417172d26c575
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-03-13 13:56:34 +0100

    Updated UploadData component

--- Components/UploadData/index.js
+++ Components/UploadData/index.js
@@ -2,60 +2,20 @@ import Heading from "../Heading";
 import { Card } from "../Card/card.styled";
 import Paragraph from "../Paragraph";
 import CenteredDiv from "../CenteredDiv";
-import CorrectArrays from "../CorrectArrays";
 import FileUploader from "../FileUploader";
-import { useState } from "react";
-import useLocalStorageState from "use-local-storage-state";
-const { convertCSVToArray } = require("convert-csv-to-array");
 import { StyledInputTypeSubmit } from "../StyledInputTypeSubmit";
 
-export default function UploadData() {
-  //State to store keys from the CSV file
-  const [keynames, setKeynames] = useLocalStorageState("keynames", {
-    defaultValue: [],
-  });
-
-  //State to store the values
-  const [vals, setVals] = useLocalStorageState("vals", {
-    defaultValue: [],
-  });
-
-  //State to store the file object
-  const [fileObj, setFileObj] = useState({});
-
-  // State to store the isUploaded status
-  const [isUploaded, setIsUploaded] = useState(false);
-
-  const handleFile = (file) => {
-    setFileObj(file);
-    setIsUploaded(true);
-  };
-
+export default function UploadData({
+  fileObj,
+  isUploaded,
+  onUploadFile,
+  onConversion,
+}) {
   // Name of file
   const fileName = fileObj.name;
 
   const handleSubmit = (event) => {
     event.preventDefault();
-  };
-
-  // Reading the file and converting the result to an array of objects
-  const handleConversion = () => {
-    const reader = new FileReader();
-
-    reader.onload = function (loadEvent) {
-      const arrayOfObjects = convertCSVToArray(loadEvent.target.result, {
-        separator: ",",
-      });
-
-      // The output of the convertCSVToArray function needs further processing
-      const [keys, ...correctValues] = CorrectArrays(arrayOfObjects);
-
-      setKeynames(keys);
-      setVals(correctValues);
-      alert("File processed successfully");
-    };
-
-    reader.readAsText(fileObj);
   };
 
   return (
@@ -75,12 +35,12 @@ export default function UploadData() {
 
       <form onSubmit={handleSubmit}>
         <CenteredDiv>
-          <FileUploader onFileUploaded={handleFile} />
+          <FileUploader onUploadFile={onUploadFile} />
           {fileName ? <p>Uploaded file: {fileName}</p> : <p>No file chosen</p>}
           <StyledInputTypeSubmit
             type="submit"
             value="Submit"
-            onClick={handleConversion}
+            onClick={onConversion}
             disabled={!isUploaded}
           />
         </CenteredDiv>
