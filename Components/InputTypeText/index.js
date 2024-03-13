baa5c653aaf2189500d07485f4d49ce3112b4968
Maria Adner <juergens.maria@googlemail.com>
2024-03-13 12:45:41 +0100

    Created InputTypeText Component

---
+++ Components/InputTypeText/index.js
@@ -0,0 +1,20 @@
+export default function InputTypeText({
+  placeholderString,
+  onChange,
+  idString,
+}) {
+  return (
+    <form>
+      <label htmlFor="idString">
+        `${idString}:`
+        <input
+          type="text"
+          id="idString"
+          placeholder="placeholderString"
+          onChange={onChange}
+          required
+        />
+      </label>
+    </form>
+  );
+}
