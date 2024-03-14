7f18cf45f1b69dbaf8e88f1c7c92b3fdc5db1194
Maria Adner <juergens.maria@googlemail.com>
2024-03-14 10:11:19 +0100

    Added XandYLabelsGraph Component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import UploadData from "../Components/UploadData";
 import ListOfCharts from "../Components/ListOfCharts";
 import Plotting from "../Components/Plotting";
+import XandYLabelsGraph from "../Components/XandYLabelsGraph";
 
 export default function HomePage({ clickedChartType, onSelectChartType }) {
   return (
     <div>
       <UploadData />
       <ListOfCharts onSelectChartType={onSelectChartType} />
+      <XandYLabelsGraph />
       <Plotting clickedChartType={clickedChartType} />
     </div>
   );
