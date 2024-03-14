337dab98a8db01c09ce9993f63b3552d9b86e601
Maria Adner <juergens.maria@googlemail.com>
2024-03-14 10:09:32 +0100

    Updated the Component respective to the other code

--- Components/Plotting/index.js
+++ Components/Plotting/index.js
@@ -4,7 +4,7 @@ import { chartArray } from "../../lib/listOfPlotTypes";
 import dynamic from "next/dynamic";
 const Plot = dynamic(() => import("react-plotly.js"), { ssr: false });
 
-export default function Plotting({ clickedChartType }) {
+export default function Plotting({ clickedChartType, XLabel, YLabel }) {
   const XVariable = [1, 2, 3, 4, 6, 8, 10, 12, 14, 16, 18, 20];
   const YVariable = [32, 37, 40.5, 43, 49, 54, 59, 63.5, 69.5, 73, 74, null];
   const match = chartArray.findIndex(
@@ -36,10 +36,12 @@ export default function Plotting({ clickedChartType }) {
               ]}
               layout={{
                 xaxis: {
+                  title: XLabel,
                   showline: true,
                   ticks: "outside",
                 },
                 yaxis: {
+                  title: YLabel,
                   ticks: "outside",
                 },
                 width: 600,
