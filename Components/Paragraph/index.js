7febecf4339fafea1c58ff0f5803155099684825
YavorLalev <yavorlalev@abv.bg>
2024-03-14 09:55:34 +0100

    added prop variant=start to align the text in Paragraph component

--- Components/Paragraph/index.js
+++ Components/Paragraph/index.js
@@ -2,8 +2,21 @@ import styled from "styled-components";
 
 const StyledP = styled.p`
   text-align: center;
+  font-weight: 400;
+  line-height: 1.2rem;
+  font-size: 1.1rem;
+
+  ${(props) =>
+    props.$variant === "start" &&
+    css`
+      text-align: start;
+      font-weight: 400;
+      line-height: 1.2rem;
+      font-size: 1.1rem;
+      padding: 10px;
+    `};
 `;
 
-export default function Paragraph({ children }) {
-  return <StyledP>{children}</StyledP>;
+export default function Paragraph({ children, $variant }) {
+  return <StyledP $variant={$variant}>{children}</StyledP>;
 }
