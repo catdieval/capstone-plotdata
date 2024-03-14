4fec8b92f6a567aacfed3523cbf03b878908abdb
Maria Adner <juergens.maria@googlemail.com>
2024-03-14 21:15:27 +0100

    Added props

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,29 @@ import ListOfCharts from "../Components/ListOfCharts";
 import Plotting from "../Components/Plotting";
 import XandYLabelsGraph from "../Components/XandYLabelsGraph";
 
-export default function HomePage({ clickedChartType, onSelectChartType }) {
+export default function HomePage({
+  clickedChartType,
+  onSelectChartType,
+  handleXLabelChange,
+  handleYLabelChange,
+  hasEnteredYLabel,
+  XLabel,
+  YLabel,
+}) {
   return (
     <div>
       <UploadData />
       <ListOfCharts onSelectChartType={onSelectChartType} />
-      <XandYLabelsGraph />
-      <Plotting clickedChartType={clickedChartType} />
+      <XandYLabelsGraph
+        handleXLabelChange={handleXLabelChange}
+        handleYLabelChange={handleYLabelChange}
+        hasEnteredYLabel={hasEnteredYLabel}
+      />
+      <Plotting
+        clickedChartType={clickedChartType}
+        XLabel={XLabel}
+        YLabel={YLabel}
+      />
     </div>
   );
 }
