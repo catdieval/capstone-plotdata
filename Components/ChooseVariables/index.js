41726e308a0f0a4538bee230bb50439f66598202
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-03-14 09:59:30 +0100

    Added states to ChooseVariables component

--- Components/ChooseVariables/index.js
+++ Components/ChooseVariables/index.js
@@ -1,1 +1,22 @@
-export default function ChooseVariables({ keynames, vals }) {}
+import useLocalStorageState from "use-local-storage-state";
+import { useState } from "react";
+
+export default function ChooseVariables({ keynames, vals }) {
+  const [xVariable, setXVariable] = useLocalStorageState("xVariable", {
+    defaultValue: [],
+  });
+
+  const [yVariable, setYVariable] = useLocalStorageState("yVariable", {
+    defaultValue: [],
+  });
+
+  const [xKey, setXKey] = useLocalStorageState("xKey", {
+    defaultValue: "",
+  });
+
+  const [yKey, setYKey] = useLocalStorageState("yKey", {
+    defaultValue: "",
+  });
+
+  const [hasChosenYVariable, setHasChosenYVariable] = useState(false);
+}
