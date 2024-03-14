2d456fa58dfa00e79ab7db9d301ff19f3c1eddc7
YavorLalev <yavorlalev@abv.bg>
2024-03-14 11:06:38 +0100

    added hover effect and fixed styling to the StyledInputTypeSubmit component

--- Components/StyledInputTypeSubmit/index.js
+++ Components/StyledInputTypeSubmit/index.js
@@ -3,11 +3,15 @@ import styled from "styled-components";
 export const StyledInputTypeSubmit = styled.input.attrs({
   type: "submit",
 })`
-  background-color: deepskyblue;
-  padding: 10px;
+  background-color: var(--secondary-color);
+  padding: 10px 20px;
   border-radius: 12px;
   border: none;
   cursor: pointer;
-  font-size: 16px;
-  font-weight: bold;
+  font-size: 1rem;
+  font-weight: 700;
+
+  &:hover {
+    background-color: #00ace6;
+  }
 `;
