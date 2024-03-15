76bfb8a33fe6b21fbe9c603631b071b98d1850ca
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-03-15 14:51:24 +0100

    Updated _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -11,10 +11,10 @@ export default function App({ Component, pageProps }) {
   const [keynames, setKeynames] = useState([]);
 
   //State to store the values
-  const [vals, setVals] = useLocalStorageState("vals", {
+  /*const [vals, setVals] = useLocalStorageState("vals", {
     defaultValue: [],
-  });
-  //const [vals, setVals] = useState([]);
+  }); */
+  const [vals, setVals] = useState([]);
 
   // State to store the file object
   const [fileObj, setFileObj] = useState({});
@@ -24,6 +24,64 @@ export default function App({ Component, pageProps }) {
 
   // State to store the selected chart type
   const [clickedChartType, setClickedChartType] = useState("");
+
+  // State to store the values of the x variable
+  const [xVariable, setXVariable] = useState([]);
+
+  // State to store the values of the y variable
+  const [yVariable, setYVariable] = useState([]);
+
+  // State to store the value selected by the user in the dropdown menu for the x variable
+  const [xKey, setXKey] = useState("");
+
+  // State to store the value selected by the user in the dropdown menu for the x variable
+  const [yKey, setYKey] = useState("");
+
+  // State to store the status if the user has chosen the y variable
+  const [hasChosenYVariable, setHasChosenYVariable] = useState(false);
+
+  function handleXChange(event) {
+    const choice = event.target.value;
+    if (choice != "") {
+      setXKey(choice);
+    }
+  }
+
+  function handleYChange(event) {
+    const choice = event.target.value;
+    if (choice != "") {
+      setYKey(choice);
+      setHasChosenYVariable(true);
+    }
+  }
+
+  function handleAssignVariables() {
+    if (vals.length > 0 && xKey != "" && yKey != "") {
+      var tempXArray = new Array(vals.length);
+      var tempYArray = new Array(vals.length);
+
+      // In tempXArray fill in with the values associated to the key in vals
+      // corresponding to the XKey value.
+      // In tempYArray fill in with the values associated to the key in vals
+      // corresponding to the YKey value.
+
+      for (let i = 0; i < vals.length; i++) {
+        for (let key in vals[i]) {
+          if (key == xKey) {
+            tempXArray[i] = vals[i][key];
+            console.log(`Index ${i}: ${tempXArray[i]}`);
+          }
+          if (key == yKey) {
+            tempYArray[i] = vals[i][key];
+          }
+        }
+      }
+
+      setXVariable(tempXArray);
+      setYVariable(tempYArray);
+      alert("Data for the x and y variables are assigned.");
+    }
+  }
 
   const handleUploadFile = (file) => {
     setFileObj(file);
@@ -49,7 +107,7 @@ export default function App({ Component, pageProps }) {
     reader.readAsText(fileObj);
   };
 
-  function handleChartType(element) {
+  function handleSelectChartType(element) {
     setClickedChartType(element.target.innerText);
     alert(`You chose ${element.target.innerText}`);
   }
@@ -61,13 +119,20 @@ export default function App({ Component, pageProps }) {
         <Component
           {...pageProps}
           keynames={keynames}
-          vals={vals}
           fileObj={fileObj}
           isUploaded={isUploaded}
           onUploadFile={handleUploadFile}
           onConversion={handleConversion}
           clickedChartType={clickedChartType}
-          onSelectChartType={handleChartType}
+          xKey={xKey}
+          yKey={yKey}
+          xVariable={xVariable}
+          yVariable={yVariable}
+          hasChosenYVariable={hasChosenYVariable}
+          onXChange={handleXChange}
+          onYChange={handleYChange}
+          onAssignVariables={handleAssignVariables}
+          onSelectChartType={handleSelectChartType}
         />
       </Layout>
     </>
