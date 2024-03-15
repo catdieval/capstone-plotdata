1fd495a32c2d71a7ccd26386869d957546d1649b
Maria Adner <juergens.maria@googlemail.com>
2024-03-15 11:23:37 +0100

    Added conditional rendering

--- Components/XandYLabelsGraph/index.js
+++ Components/XandYLabelsGraph/index.js
@@ -10,6 +10,9 @@ export default function XandYLabelsGraph({
   handleYLabelChange,
   hasEnteredYLabel,
 }) {
+  const xKey = "Age";
+  const yKey = "Height";
+
   function handleSubmit(event) {
     event.preventDefault();
   }
@@ -20,41 +23,47 @@ export default function XandYLabelsGraph({
 
   return (
     <>
-      <Heading>Step 4: Give labels to axes</Heading>
-      <Paragraph>
-        For the x variable you chose:<br></br>
-        {"Age"}
-      </Paragraph>
-      <Paragraph>
-        For the y variable you chose:<br></br>
-        {"Height"}
-      </Paragraph>
-      <CenteredDiv>
-        <form onSubmit={handleSubmit}>
-          <InputTypeText
-            idString={"Label for the x-axis"}
-            placeholderString={"Fill me with name and unit, e.g. Age (years)"}
-            onChange={handleXLabelChange}
-          />
-          <br></br>
-          <br></br>
-          <InputTypeText
-            idString={"Label for the y-axis"}
-            placeholderString={
-              "Fill me with name and unit, e.g. Height (inches)"
-            }
-            onChange={handleYLabelChange}
-          />
-          <br></br>
-          <br></br>
-          <StyledInputTypeSubmit
-            type="submit"
-            value="Next"
-            onClick={handleAxesLabels}
-            disabled={!hasEnteredYLabel}
-          />
-        </form>
-      </CenteredDiv>
+      {xKey != "" && yKey != "" ? (
+        <>
+          <Heading>Step 4: Give labels to axes</Heading>
+          <Paragraph>
+            For the x variable you chose:<br></br>
+            {xKey}
+          </Paragraph>
+          <Paragraph>
+            For the y variable you chose:<br></br>
+            {yKey}
+          </Paragraph>
+          <CenteredDiv>
+            <form onSubmit={handleSubmit}>
+              <InputTypeText
+                idString={"Label for the x-axis"}
+                placeholderString={
+                  "Fill me with name and unit, e.g. Age (years)"
+                }
+                onChange={handleXLabelChange}
+              />
+              <br></br>
+              <br></br>
+              <InputTypeText
+                idString={"Label for the y-axis"}
+                placeholderString={
+                  "Fill me with name and unit, e.g. Height (inches)"
+                }
+                onChange={handleYLabelChange}
+              />
+              <br></br>
+              <br></br>
+              <StyledInputTypeSubmit
+                type="submit"
+                value="Next"
+                onClick={handleAxesLabels}
+                disabled={!hasEnteredYLabel}
+              />
+            </form>
+          </CenteredDiv>
+        </>
+      ) : null}
     </>
   );
 }
