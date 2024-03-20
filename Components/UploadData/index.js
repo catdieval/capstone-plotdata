f396483bf01a26e73590a3980ed46303032dd722
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-03-20 11:23:38 +0100

    Updated UploadData component

--- Components/UploadData/index.js
+++ Components/UploadData/index.js
@@ -20,7 +20,7 @@ export default function UploadData({
 
   return (
     <>
-      <Heading>Step 1: Upload a CSV file to get the data.</Heading>
+      <Heading>Step 1: Upload a CSV file to get the data</Heading>
       <Card>
         <Paragraph>
           <b>Notes:</b>
