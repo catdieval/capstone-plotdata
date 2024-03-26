d0326653a3e7787933414622181b0be7ba8bae4a
YavorLalev <yavorlalev@abv.bg>
2024-03-26 11:15:55 +0100

    added form and handleSubmit function

--- Components/LineProperties/index.js
+++ Components/LineProperties/index.js
@@ -1,11 +1,17 @@
 import Heading from "../Heading";
 import { useState } from "react";
+import { Card } from "../Card/card.styled";
+import { Container } from "../Container";
+import Paragraph from "../Paragraph";
 
 export default function LineProperties() {
   const [lineColor, setLineColor] = useState("");
   const [lineStyle, setLineStyle] = useState("");
   const [lineWidth, setLineWidth] = useState("");
 
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
   function handleLineColorChange(event) {
     setLineColor(event.target.value);
   }
@@ -17,5 +23,17 @@ export default function LineProperties() {
     setLineWidth(event.target.value);
   }
 
-  return <Heading>Line Properties</Heading>;
+  return (
+    <Container $centered="center">
+      <Heading>Line Properties</Heading>
+      <form $variant="form" onSubmit={handleSubmit}>
+        <Container>
+          {" "}
+          <Paragraph> Line color</Paragraph>
+          <Paragraph> Line style</Paragraph>
+          <Paragraph> Line width</Paragraph>
+        </Container>
+      </form>
+    </Container>
+  );
 }
