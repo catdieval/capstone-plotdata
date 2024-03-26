8ceba76d38cf7a8f684a37a08ba1a3c7be38fe6e
YavorLalev <yavorlalev@abv.bg>
2024-03-26 11:04:13 +0100

    added handleChange functions

--- Components/LineProperties/index.js
+++ Components/LineProperties/index.js
@@ -6,5 +6,16 @@ export default function LineProperties() {
   const [lineStyle, setLineStyle] = useState("");
   const [lineWidth, setLineWidth] = useState("");
 
+  function handleLineColorChange(event) {
+    setLineColor(event.target.value);
+  }
+
+  function handleLineStyleChange(event) {
+    setLineStyle(event.target.value);
+  }
+  function handleLineWidthChange(event) {
+    setLineWidth(event.target.value);
+  }
+
   return <Heading>Line Properties</Heading>;
 }
