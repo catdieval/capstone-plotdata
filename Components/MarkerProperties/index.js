5953616cd5f46bccea36962d51690445aca975bc
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-03-26 11:09:00 +0100

    Added handle functions for markerColor, markerSymbol, markerSize to MarkerProperties

--- Components/MarkerProperties/index.js
+++ Components/MarkerProperties/index.js
@@ -4,4 +4,19 @@ export default function MarkerProperties() {
   const [markerColor, setMarkerColor] = useState("");
   const [markerSymbol, setMarkerSymbol] = useState("");
   const [markerSize, setMarkerSize] = useState(0);
+
+  function handleMarkerColorChange(event) {
+    const choice = event.target.value;
+    setMarkerColor(choice);
+  }
+
+  function handleMarkerSymbolChange(event) {
+    const choice = event.target.value;
+    setMarkerSymbol(choice);
+  }
+
+  function handleMarkerSize(event) {
+    const choice = event.target.value;
+    setMarkerSize(choice);
+  }
 }
