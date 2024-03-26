dfecd4d15fa49a4aa37c5d1c7bea8a88ded07845
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-03-26 11:02:11 +0100

    Added states for markerColor, markerSymbol, markerSize to MarkerProperties

---
+++ Components/MarkerProperties/index.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+export default function MarkerProperties() {
+  const [markerColor, setMarkerColor] = useState("");
+  const [markerSymbol, setMarkerSymbol] = useState("");
+  const [markerSize, setMarkerSize] = useState(0);
+}
