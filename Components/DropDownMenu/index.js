96c295c2e5bb667a8763c8745f9e6c20c6bc6e11
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-04-10 09:36:49 +0200

    Merged conflict solved in DropDownMenu

--- Components/DropDownMenu/index.js
+++ Components/DropDownMenu/index.js
@@ -6,18 +6,22 @@ const StyledLabel = styled.label`
   margin: 0.3rem;
 `;
 
+const StyledSelect = styled.select`
+  width: 100px;
+`;
+
 export default function DropDownMenu({ idString, onChange, arrayOfOptions }) {
   return (
     <Container $direction="column" $center>
       <StyledLabel htmlFor={idString}>{idString}</StyledLabel>
-      <select required name={idString} id={idString} onChange={onChange}>
+      <StyledSelect required name={idString} id={idString} onChange={onChange}>
         <option value="">Select</option>
         {arrayOfOptions.map((optionName) => (
           <option key={optionName} value={optionName}>
             {optionName}
           </option>
         ))}
-      </select>
+      </StyledSelect>
     </Container>
   );
 }
