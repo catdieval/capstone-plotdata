770268e218f7281d4a3ef218065c26043e3eb23b
Maria Adner <juergens.maria@googlemail.com>
2024-04-10 12:47:14 +0200

    Created GridProperties component

---
+++ Components/GridProperties/index.js
@@ -0,0 +1,80 @@
+import InputTypeRadio from "../InputTypeRadio";
+import Paragraph from "../Paragraph";
+import { Container } from "../Container";
+import DropDownMenu from "../DropDownMenu";
+import { lineStyleArray } from "../../lib/listOfLineProperties";
+
+export default function GridProperties({ grid, onGridChange }) {
+  return (
+    <>
+      <Paragraph>Show grid (helper lines) for axis: </Paragraph>
+      <Container $wrap="wrap">
+        <Container $centered="center">
+          <Paragraph>For x-Axis:</Paragraph>
+          <Container $row="row">
+            <InputTypeRadio
+              name="gridXAxis"
+              value="true"
+              idString={"yes"}
+              onChange={onGridChange}
+              label="Yes"
+            />
+            <InputTypeRadio
+              name="gridXAxis"
+              value="false"
+              idString={"no"}
+              onChange={onGridChange}
+              label="No"
+            />
+          </Container>
+        </Container>
+        <Container $centered="center">
+          <Paragraph>For y-Axis:</Paragraph>
+          <Container $row="row">
+            <InputTypeRadio
+              name="gridYAxis"
+              value="true"
+              idString={"yes"}
+              onChange={onGridChange}
+              label="Yes"
+            />
+            <InputTypeRadio
+              name="gridYAxis"
+              value="false"
+              idString={"no"}
+              onChange={onGridChange}
+              label="No"
+            />
+          </Container>
+        </Container>
+      </Container>
+      {console.log(grid)}
+      {(grid.gridXAxis === "true" || grid.gridYAxis === "true") && (
+        <Paragraph>Grid Linestyle:</Paragraph>
+      )}
+      <Container $wrap="wrap">
+        {grid.gridXAxis === "true" && (
+          <>
+            <DropDownMenu
+              idString="x-axis"
+              nameString="gridLineStyleXAxis"
+              arrayOfOptions={lineStyleArray}
+              onChange={onGridChange}
+            />
+          </>
+        )}
+
+        {grid.gridYAxis === "true" && (
+          <>
+            <DropDownMenu
+              idString="y-axis"
+              nameString="gridLineStyleYAxis"
+              arrayOfOptions={lineStyleArray}
+              onChange={onGridChange}
+            />
+          </>
+        )}
+      </Container>
+    </>
+  );
+}
