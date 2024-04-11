bf372712689961588a489a22bb4d78e21d0cd6c7
Maria Adner <juergens.maria@googlemail.com>
2024-04-11 10:27:36 +0200

    InputTypeRadio component

---
+++ Components/InputTypeRadio/index.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export default function InputTypeRadio({
+  nameString,
+  valueString,
+  onChange,
+  idString,
+  labelString,
+}) {
+  function checked() {
+    nameString === valueString;
+  }
+
+  return (
+    <>
+      <RadioButton>
+        <input
+          type="radio"
+          name={nameString}
+          value={valueString}
+          id={idString}
+          onChange={onChange}
+          checked={checked()}
+        />
+        <Label htmlFor={idString}>{labelString}</Label>
+      </RadioButton>
+    </>
+  );
+}
+
+const RadioButton = styled.div`
+  display: flex;
+`;
+
+const Label = styled.label`
+  display: flex;
+  gap: 10rem;
+  flex-direction: row;
+`;
