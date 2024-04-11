dcf18f729028e42b8af2576d2b9671cdd838995b
YavorLalev <yavorlalev@abv.bg>
2024-04-11 11:29:55 +0200

    added state function

--- Components/LogScaleProperties/index.js
+++ Components/LogScaleProperties/index.js
@@ -1,6 +1,13 @@
 import DialogBox from "../DialogBox";
+import { useState } from "react";
 
 export default function LogScaleProperties() {
+  const initialLogScaleStates = {
+    logXAxis: "",
+    logYAxis: "",
+  };
+
+  const [logScaleAxis, setLogScaleAxis] = useState(initialLogScaleStates);
   return (
     <>
       <DialogBox />
