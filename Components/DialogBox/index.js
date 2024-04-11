2c6556b7d7c59f2ed5ea9cfa49e3c305fe8ef9fa
YavorLalev <yavorlalev@abv.bg>
2024-04-11 09:35:09 +0200

    added openDialog state and handle functions

--- Components/DialogBox/index.js
+++ Components/DialogBox/index.js
@@ -1,1 +1,11 @@
-export default function DialogBox() {}
+export default function DialogBox() {
+  const [openDialog, setOpenDialog] = useState(false);
+
+  function handleOpenDialog() {
+    setOpenDialog(!openDialog);
+  }
+
+  function handleCloseDialog() {
+    setOpenDialog();
+  }
+}
