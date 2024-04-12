535f66537d3228b293fbbb9ed5b4e1fa23dce35a
YavorLalev <yavorlalev@abv.bg>
2024-04-12 11:27:15 +0200

    added css positioning

--- Components/LogScaleProperties/index.js
+++ Components/LogScaleProperties/index.js
@@ -2,6 +2,18 @@ import DialogBox from "../DialogBox";
 import { useState } from "react";
 import Paragraph from "../Paragraph";
 import { Container } from "../Container";
+import styled from "styled-components";
+
+const MainWrapper = styled.div`
+  position: relative;
+`;
+
+const InfoButtonWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(370%, -50%);
+`;
 
 export default function LogScaleProperties() {
   const initialLogScaleStates = {
@@ -19,10 +31,16 @@ export default function LogScaleProperties() {
   }
   return (
     <>
-      <Container $direction="row">
-        <Paragraph $variant="border">Logarithmic scale for the axes</Paragraph>
-        <DialogBox />
-      </Container>
+      <MainWrapper>
+        <Container $centered="center">
+          <Paragraph $variant="border">
+            Logarithmic scale for the axes
+          </Paragraph>
+          <InfoButtonWrapper>
+            <DialogBox />
+          </InfoButtonWrapper>
+        </Container>
+      </MainWrapper>
     </>
   );
 }
