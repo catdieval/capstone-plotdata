66144f12441a9166a5486d55b4984c2b9a43252f
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-04-15 12:32:45 +0200

    Added Button and UnorderedList to the return of StartPage component

--- Components/StartPage/index.js
+++ Components/StartPage/index.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { Container } from "../Container";
 import Paragraph from "../Paragraph";
 import { StyledH3 } from "../Heading";
+import UnorderedList from "../UnorderedList";
+import Button from "../Button";
+import { arrayOfSteps, arrayOfActions } from "../../lib/listOfBulletPoints";
 
 export default function StartPage() {
   const [hasClickedGetStarted, setHasClickedGetStarted] = useState(false);
@@ -16,7 +19,11 @@ export default function StartPage() {
         With PlotData you can easily make customisable publication-quality
         graphs, all without programming skills.
       </Paragraph>
+      <Container $wrap="wrap"></Container>
       <StyledH3>Overview:</StyledH3>
+      <Container $centered="center">
+        <UnorderedList $arrayOfBulletPoints={arrayOfSteps} />
+      </Container>
       <Paragraph>
         Et voila! An interactive graph gets generated, with the help of the{" "}
         <a href="https://plotly.com/javascript/basic-charts/" target="_blank">
@@ -24,6 +31,10 @@ export default function StartPage() {
         </a>
         .
       </Paragraph>
+      <Container $centered="center">
+        <UnorderedList $arrayOfBulletPoints={arrayOfActions} />
+        <Button onClick={handleGetStarted}>Get started</Button>
+      </Container>
     </>
   );
 }
