35b34cb31b0c926532c4d71547087079680f26b8
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-04-15 11:58:39 +0200

    Added Paragraph, Container and StyledH3 to the return of StartPage component

--- Components/StartPage/index.js
+++ Components/StartPage/index.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { Container } from "../Container";
+import Paragraph from "../Paragraph";
+import { StyledH3 } from "../Heading";
 
 export default function StartPage() {
   const [hasClickedGetStarted, setHasClickedGetStarted] = useState(false);
@@ -6,4 +9,21 @@ export default function StartPage() {
   function handleGetStarted() {
     setHasClickedGetStarted(true);
   }
+
+  return (
+    <>
+      <Paragraph>
+        With PlotData you can easily make customisable publication-quality
+        graphs, all without programming skills.
+      </Paragraph>
+      <StyledH3>Overview:</StyledH3>
+      <Paragraph>
+        Et voila! An interactive graph gets generated, with the help of the{" "}
+        <a href="https://plotly.com/javascript/basic-charts/" target="_blank">
+          Plotly JavaScript library
+        </a>
+        .
+      </Paragraph>
+    </>
+  );
 }
