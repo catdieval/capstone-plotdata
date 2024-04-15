93d94e7d67066989b96588fda6a076febc89b4e2
YavorLalev <yavorlalev@abv.bg>
2024-04-15 10:40:55 +0200

    fixed Plotting component

--- Components/Plotting/index.js
+++ Components/Plotting/index.js
@@ -24,6 +24,7 @@ export default function Plotting({
   const chartIndex = chartArray.findIndex(
     (chart) => chart.name === clickedChartType
   );
+
   let selectedMode, selectedType;
 
   if (chartIndex != -1) {
