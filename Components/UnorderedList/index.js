2877d90869e7a472cf393412dde65d1f638ce973
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-04-15 11:27:38 +0200

    Created UnorderedList component

---
+++ Components/UnorderedList/index.js
@@ -0,0 +1,9 @@
+export default function UnorderedList(arrayOfBulletPoints) {
+  return (
+    <ul>
+      {arrayOfBulletPoints.map((bulletPoint) => (
+        <li key={bulletPoint}>{bulletPoint}</li>
+      ))}
+    </ul>
+  );
+}
