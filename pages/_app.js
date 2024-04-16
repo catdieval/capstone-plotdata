7f0ae16b6afdf1b3ee4ad271f93df5968836589c
Maria Adner <juergens.maria@googlemail.com>
2024-04-16 12:34:21 +0200

    changed states to an object

--- pages/_app.js
+++ pages/_app.js
@@ -25,61 +25,37 @@ export default function App({ Component, pageProps }) {
   const [xVariable, setXVariable] = useState([]);
   const [yVariable, setYVariable] = useState([]);
 
-  // States to store the values selected by the user in the dropdown menus for the x and y variables
-  const [xKey, setXKey] = useState("");
-  const [yKey, setYKey] = useState("");
-
-  //Step 4: Labels to the axes:
-  const [xLabel, setXLabel] = useState("");
-  const [yLabel, setYLabel] = useState("");
-  const [hasCompletedStep4, setHasCompletedStep4] = useState(false);
-
-  //Step 5: Add a title to the graph
-  const [titleLabel, setTitleLabel] = useState("");
-  const [hasCompletedStep5, setHasCompletedStep5] = useState(false);
-
-  //Step 6: Plotting properties
-
-  // Bar properties
-  const [barColor, setBarColor] = useState("");
-
-  //Line properties
-  const [lineColor, setLineColor] = useState("");
-  const [lineStyle, setLineStyle] = useState("");
-  const [lineWidth, setLineWidth] = useState(0);
-
-  // Marker Properties
-  const [markerColor, setMarkerColor] = useState("");
-  const [markerSymbol, setMarkerSymbol] = useState("");
-  const [markerSize, setMarkerSize] = useState(0);
-
-  // Log Scale Properties
-  const initialLogScaleStates = {
-    logXAxis: "",
-    logYAxis: "",
-  };
-  const [logScaleAxes, setLogScaleAxes] = useState(initialLogScaleStates);
-
-  //Layout Properties
-  const initialGridProperties = {
+  //Step 4, 5 and 6:
+  const initialSettings = {
+    xKey: "",
+    yKey: "",
+    xLabel: "",
+    yLabel: "",
+    titleLabel: "",
+    barColor: "",
+    lineColor: "",
+    lineStyle: "",
+    lineWidth: 0,
+    markerColor: "",
+    markerSymbol: 0,
     gridXAxis: "",
     gridYAxis: "",
     gridLineStyleXAxis: "",
     gridLineStyleYAxis: "",
-  };
-
-  const [grid, setGrid] = useState(initialGridProperties);
-
-  const initialRange = {
     rangeXAxis: "",
     rangeYAxis: "",
     minXAxis: "",
     maxXAxis: "",
     minYAxis: "",
     maxYAxis: "",
+    logXAxis: "",
+    logYAxis: "",
   };
 
-  const [range, setRange] = useState(initialRange);
+  const [settings, setSettings] = useState(initialSettings);
+  function handleSettings(event) {
+    setSettings({ ...settings, [event.target.name]: event.target.value });
+  }
 
   function handleUploadFile(file) {
     setFileObj(file);
@@ -106,14 +82,6 @@ export default function App({ Component, pageProps }) {
 
   function handleSelectChartType(name) {
     setClickedChartType(name);
-  }
-
-  function handleXChange(event) {
-    setXKey(event.target.value);
-  }
-
-  function handleYChange(event) {
-    setYKey(event.target.value);
   }
 
   function handleAssignVariables() {
@@ -143,70 +111,14 @@ export default function App({ Component, pageProps }) {
     }
   }
 
-  function handleXLabelChange(event) {
-    setXLabel(event.target.value);
-  }
-
-  function handleYLabelChange(event) {
-    setYLabel(event.target.value);
-  }
-
   function handleHasCompletedStep4() {
     alert("Labels for the x-axis and the y-axis of the graph are assigned");
     setHasCompletedStep4(true);
   }
 
-  function handleTitleChange(event) {
-    setTitleLabel(event.target.value);
-  }
-
   function handleHasCompletedStep5() {
     alert("Title of the graph is assigned");
     setHasCompletedStep5(true);
-  }
-
-  function handleLineColorChange(event) {
-    setLineColor(event.target.value);
-  }
-
-  function handleLineStyleChange(event) {
-    setLineStyle(event.target.value);
-  }
-
-  function handleLineWidthChange(event) {
-    setLineWidth(event.target.value);
-  }
-
-  function handleBarColorChange(event) {
-    const choice = event.target.value;
-    setBarColor(choice);
-  }
-
-  function handleMarkerColorChange(event) {
-    setMarkerColor(event.target.value);
-  }
-
-  function handleMarkerSymbolChange(event) {
-    setMarkerSymbol(event.target.value);
-  }
-
-  function handleMarkerSizeChange(event) {
-    setMarkerSize(event.target.value);
-  }
-
-  function handleGridProperties(event) {
-    setGrid({ ...grid, [event.target.name]: event.target.value });
-  }
-
-  function handleRange(event) {
-    setRange({ ...range, [event.target.name]: event.target.value });
-  }
-
-  function handleLogAxes(event) {
-    setLogScaleAxes({
-      ...logScaleAxes,
-      [event.target.name]: event.target.value,
-    });
   }
 
   return (
@@ -220,43 +132,17 @@ export default function App({ Component, pageProps }) {
         onConversion={handleConversion}
         clickedChartType={clickedChartType}
         onSelectChartType={handleSelectChartType}
-        xKey={xKey}
-        yKey={yKey}
         xVariable={xVariable}
         yVariable={yVariable}
         onXChange={handleXChange}
         onYChange={handleYChange}
         onAssignVariables={handleAssignVariables}
-        xLabel={xLabel}
-        yLabel={yLabel}
-        onXLabelChange={handleXLabelChange}
-        onYLabelChange={handleYLabelChange}
         hasCompletedStep4={hasCompletedStep4}
         onHasCompletedStep4={handleHasCompletedStep4}
-        titleLabel={titleLabel}
-        onTitleChange={handleTitleChange}
         hasCompletedStep5={hasCompletedStep5}
         onHasCompletedStep5={handleHasCompletedStep5}
-        barColor={barColor}
-        onBarColorChange={handleBarColorChange}
-        lineColor={lineColor}
-        lineStyle={lineStyle}
-        lineWidth={lineWidth}
-        onLineColorChange={handleLineColorChange}
-        onLineStyleChange={handleLineStyleChange}
-        onLineWidthChange={handleLineWidthChange}
-        markerColor={markerColor}
-        markerSymbol={markerSymbol}
-        markerSize={markerSize}
-        onMarkerColorChange={handleMarkerColorChange}
-        onMarkerSymbolChange={handleMarkerSymbolChange}
-        onMarkerSizeChange={handleMarkerSizeChange}
-        grid={grid}
-        onGridChange={handleGridProperties}
-        range={range}
-        onRangeChange={handleRange}
-        onLogScaleChange={handleLogAxes}
-        logScaleAxes={logScaleAxes}
+        settings={settings}
+        onSettingsChange={handleSettings}
       />
     </Layout>
   );
