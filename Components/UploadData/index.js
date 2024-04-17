2b7c4b0b553ad692626267cd901dbc185eb6a717
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-04-17 11:36:40 +0200

    Added conditional rendering to the return of the UploadData component

--- Components/UploadData/index.js
+++ Components/UploadData/index.js
@@ -5,7 +5,12 @@ import { Container } from "../Container";
 import FileUploader from "../FileUploader";
 import { StyledInputTypeSubmit } from "../StyledInputTypeSubmit";
 
-export default function UploadData({ fileObj, onUploadFile, onConversion }) {
+export default function UploadData({
+  fileObj,
+  onUploadFile,
+  onConversion,
+  hasClickedGetStarted,
+}) {
   // Name of file
   const fileName = fileObj?.name;
 
@@ -15,34 +20,41 @@ export default function UploadData({ fileObj, onUploadFile, onConversion }) {
 
   return (
     <>
-      <Heading>Step 1: Upload a CSV file</Heading>
-      <Card>
-        <Paragraph $variant="bold">Requirements:</Paragraph>
-        <Paragraph $variant="start">
-          <span> ✔ </span> The file should have a header.
-        </Paragraph>
-        <Paragraph $variant="start">
-          <span> ✔ </span>The file should be comma-delimited and the decimal
-          separator for numbers should be a period (.).
-        </Paragraph>
-        <Paragraph $variant="start">
-          <span> ✔ </span> If the file contains missing values, then replace
-          these values in the file by null.
-        </Paragraph>
-      </Card>
-
-      <form onSubmit={handleSubmit}>
-        <Container $centered="center" $margin>
-          <FileUploader onUploadFile={onUploadFile} />
-          {fileName ? <p>Uploaded file: {fileName}</p> : <p>No file chosen</p>}
-          <StyledInputTypeSubmit
-            $nomargin
-            value="Next"
-            onClick={onConversion}
-            disabled={fileObj === null || fileObj === undefined}
-          />
-        </Container>
-      </form>
+      {hasClickedGetStarted === true && (
+        <>
+          <Heading>Step 1: Upload a CSV file</Heading>
+          <Card>
+            <Paragraph $variant="bold">Requirements:</Paragraph>
+            <Paragraph $variant="start">
+              <span> ✔ </span> The file should have a header.
+            </Paragraph>
+            <Paragraph $variant="start">
+              <span> ✔ </span>The file should be comma-delimited and the decimal
+              separator for numbers should be a period (.).
+            </Paragraph>
+            <Paragraph $variant="start">
+              <span> ✔ </span> If the file contains missing values, then replace
+              these values in the file by null.
+            </Paragraph>
+          </Card>
+          <form onSubmit={handleSubmit}>
+            <Container $centered="center" $margin>
+              <FileUploader onUploadFile={onUploadFile} />
+              {fileName ? (
+                <p>Uploaded file: {fileName}</p>
+              ) : (
+                <p>No file chosen</p>
+              )}
+              <StyledInputTypeSubmit
+                $nomargin
+                value="Next"
+                onClick={onConversion}
+                disabled={fileObj === null || fileObj === undefined}
+              />
+            </Container>
+          </form>
+        </>
+      )}
     </>
   );
 }
