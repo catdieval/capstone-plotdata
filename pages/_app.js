b7d275f1e25149450a84cb00e9d806da756d1b03
Maria Adner <juergens.maria@googlemail.com>
2024-04-17 10:04:46 +0200

    changed words

--- pages/_app.js
+++ pages/_app.js
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }) {
   const [xKey, setXKey] = useState("");
   const [yKey, setYKey] = useState("");
 
-  //Step 4 (Label), 5 (Title) and 6 (Properties):
+  // Step 4 (Axes labels), 5 (Title) and 6 (Properties):
   const initialSettings = {
     xLabel: "",
     yLabel: "",
