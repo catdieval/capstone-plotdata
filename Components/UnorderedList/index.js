c16b1c4747b0e609969d2c511e2c2768739ca1a0
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-04-22 12:17:24 +0200

    Added a styling for the li element in the UnorderedList component

--- Components/UnorderedList/index.js
+++ Components/UnorderedList/index.js
@@ -1,11 +1,16 @@
 import { Container } from "../Container";
+import styled from "styled-components";
 
 export default function UnorderedList({ $arrayOfBulletPoints }) {
+  const StyledLi = styled.li`
+    font-size: 1.1rem;
+  `;
+
   return (
     <ul>
       <Container $direction="column" $gap>
         {$arrayOfBulletPoints.map((bulletPoint) => {
-          return <li key={bulletPoint}>{bulletPoint}</li>;
+          return <StyledLi key={bulletPoint}>{bulletPoint}</StyledLi>;
         })}
       </Container>
     </ul>
