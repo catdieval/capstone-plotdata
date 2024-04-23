b1035a19a7a1ab1a0ae9c68956abd079f2998ca9
Maria Adner <juergens.maria@googlemail.com>
2024-04-23 11:02:44 +0200

    changed filename

---
+++ Components/InputTypeText/styledInputTypeText.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledInput = styled.input.attrs({
+  type: "text",
+})`
+  width: 50%;
+  padding: 0.5rem 1rem;
+  margin: 0.5rem 0;
+  box-shadow: 0px 1px 4px -2px rgba(0, 0, 0, 0.1);
+  border: none;
+  border-radius: 0.3rem;
+  background-color: #ececec;
+`;
