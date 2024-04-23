f5da06c1397be9d5b84a6882230f8bd9ff4b7f2f
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-04-23 16:56:33 +0200

    Corrected import of Container in the UnorderedList component

--- Components/UnorderedList/index.js
+++ Components/UnorderedList/index.js
@@ -1,4 +1,4 @@
-import { Container } from "../Container";
+import Container from "../Container";
 import styled from "styled-components";
 
 export default function UnorderedList({ $arrayOfBulletPoints }) {
