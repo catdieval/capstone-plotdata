6729f7e6443c06429e1878beb72cefad093b07a5
Catherine Dieval <catherine.dieval@hotmail.fr>
2024-04-26 12:23:34 +0200

    Solved merge conflict

--- Components/BarProperties/index.js
+++ Components/BarProperties/index.js
@@ -6,9 +6,7 @@ import { StyledH3 } from "../Heading";
 import Paragraph from "../Paragraph";
 
 export default function BarProperties({
-  clickedChartType,
   onHasCompletedDistributionProperties,
-  hasCompletedStep5,
   settings,
   onSettingsChange,
 }) {
@@ -33,7 +31,7 @@ export default function BarProperties({
           />
           <InputTypeSubmit
             value="Next"
-            onClick={onHasCompletedDistProp}
+            onClick={onHasCompletedDistributionProperties}
             disabled={notCompletedBarProperties()}
           />
         </Container>
