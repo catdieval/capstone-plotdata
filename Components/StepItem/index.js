2733e721448f353ad15e8869fbf5548df614e6b2
YavorLalev <yavorlalev@abv.bg>
2024-04-26 12:24:40 +0200

    created StepItem component

---
+++ Components/StepItem/index.js
@@ -0,0 +1,18 @@
+import Button from "../Button";
+import styled from "styled-components";
+import Paragraph from "../Paragraph";
+
+const ItemContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.8rem;
+`;
+export default function StepItem({ name, label }) {
+  return (
+    <ItemContainer>
+      <Button>{name}</Button>
+      <Paragraph $variant="bold">{label}</Paragraph>
+    </ItemContainer>
+  );
+}
