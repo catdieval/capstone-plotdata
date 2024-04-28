e2397cd778fac07aea03fdad0b17a4cedc74aac2
YavorLalev <yavorlalev@abv.bg>
2024-04-28 21:24:14 +0200

    modified ListOfCharts component

--- Components/ListOfCharts/index.js
+++ Components/ListOfCharts/index.js
@@ -10,24 +10,24 @@ export default function ListOfCharts({
 }) {
   return (
     <>
-      {keynames.length > 0 && (
-        <>
-          <Heading>Step 2: Select the type of chart</Heading>
-          <Container $wrap="wrap">
-            {chartArray.map(({ name, icon }) => {
-              return (
-                <ChartItem
-                  key={name}
-                  icon={icon}
-                  name={name}
-                  clickedChartType={clickedChartType}
-                  onSelectChartType={() => onSelectChartType(name)}
-                />
-              );
-            })}
-          </Container>
-        </>
-      )}
+      {/* {keynames.length > 0 && ( */}
+      <>
+        <Heading>Step 2: Select the type of chart</Heading>
+        <Container $wrap="wrap">
+          {chartArray.map(({ name, icon }) => {
+            return (
+              <ChartItem
+                key={name}
+                icon={icon}
+                name={name}
+                clickedChartType={clickedChartType}
+                onSelectChartType={() => onSelectChartType(name)}
+              />
+            );
+          })}
+        </Container>
+      </>
+      {/* )} */}
     </>
   );
 }
