5d1a631149591804fc2214f873505c823943f3cb
YavorLalev <yavorlalev@abv.bg>
2024-04-29 12:27:43 +0200

    refactored LyoutProperties component

--- Components/LayoutProperties/index.js
+++ Components/LayoutProperties/index.js
@@ -2,27 +2,11 @@ import GridProperties from "../GridProperties";
 import { StyledH3 } from "../Heading";
 import LogScaleProperties from "../LogScaleProperties";
 import RangeProperties from "../RangeProperties";
-import InputTypeSubmit from "../InputTypeSubmit";
 import Container from "../Container";
 
-export default function LayoutProperties({
-  settings,
-  onSettingsChange,
-  onHasCompletedStep6,
-}) {
+export default function LayoutProperties({ settings, onSettingsChange }) {
   function handleSubmit(event) {
     event.preventDefault();
-  }
-
-  function notCompletedLayoutProperties() {
-    return (
-      settings.gridXAxis.length === 0 ||
-      settings.gridYAxis.length === 0 ||
-      settings.rangeXAxis.length === 0 ||
-      settings.rangeYAxis.length === 0 ||
-      settings.logXAxis.length === 0 ||
-      settings.logYAxis.length === 0
-    );
   }
 
   return (
@@ -41,13 +25,6 @@ export default function LayoutProperties({
           settings={settings}
           onSettingsChange={onSettingsChange}
         />
-        <Container $centered="center">
-          <InputTypeSubmit
-            value="Plot"
-            onClick={onHasCompletedStep6}
-            disabled={notCompletedLayoutProperties()}
-          />
-        </Container>
       </form>
     </>
   );
