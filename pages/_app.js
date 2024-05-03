f9def1982048b842c4edd69969810c4a972a6879
YavorLalev <yavorlalev@abv.bg>
2024-05-03 22:25:40 +0200

    removed console.log

--- pages/_app.js
+++ pages/_app.js
@@ -149,7 +149,6 @@ export default function App({ Component, pageProps }) {
   function trackSteps(step) {
     setClickedSteps([...clickedSteps, step]);
   }
-  console.log(clickedSteps);
   return (
     <Layout>
       <GlobalStyle />
