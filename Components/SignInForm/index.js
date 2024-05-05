95c2802fbffed23aab2b895360cfe089e6485c77
Maria Adner <juergens.maria@googlemail.com>
2024-05-05 22:04:55 +0200

    created button

--- Components/SignInForm/index.js
+++ Components/SignInForm/index.js
@@ -1,3 +1,8 @@
 import { useSession, signIn, signOut } from "next-auth/react";
+import Button from "../Button";
 
-export default function SingInForm() {}
+export default function SingInForm() {
+  const { data: session } = useSession();
+
+  return <Button></Button>;
+}
