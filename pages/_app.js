ae3dd55a879c27e2f5484822a808305fd228492e
Maria Adner <juergens.maria@googlemail.com>
2024-05-05 21:49:16 +0200

    added session context

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,12 @@ import Layout from "../Components/Layout";
 import { useState } from "react";
 import CorrectArrays from "../Components/CorrectArrays";
 import ConvertCSVToArray from "../Components/ConvertCSVToArray";
+import { SessionProvider } from "next-auth/react";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   // State to store the status about if the user has clicked on the "Get started" button
   const [hasClickedGetStarted, setHasClickedGetStarted] = useState(false);
 
@@ -163,38 +167,42 @@ export default function App({ Component, pageProps }) {
   }
 
   return (
-    <Layout>
-      <GlobalStyle />
-      <Component
-        {...pageProps}
-        hasClickedGetStarted={hasClickedGetStarted}
-        onGetStarted={handleGetStarted}
-        keynames={keynames}
-        fileObject={fileObject}
-        onUploadFile={handleUploadFile}
-        onConversion={handleConversion}
-        clickedChartType={clickedChartType}
-        onSelectChartType={handleSelectChartType}
-        xKey={xKey}
-        yKey={yKey}
-        xVariable={xVariable}
-        yVariable={yVariable}
-        onXChange={handleXChange}
-        onYChange={handleYChange}
-        onAssignVariables={handleAssignVariables}
-        settings={settings}
-        onSettingsChange={handleSettingsChange}
-        hasCompletedStep4={hasCompletedStep4}
-        onHasCompletedStep4={handleHasCompletedStep4}
-        hasCompletedStep5={hasCompletedStep5}
-        onHasCompletedStep5={handleHasCompletedStep5}
-        hasCompletedDistributionProperties={hasCompletedDistributionProperties}
-        onHasCompletedDistributionProperties={
-          handleHasCompletedDistributionProperties
-        }
-        hasCompletedStep6={hasCompletedStep6}
-        onHasCompletedStep6={handleHasCompletedStep6}
-      />
-    </Layout>
+    <SessionProvider session={session}>
+      <Layout>
+        <GlobalStyle />
+        <Component
+          {...pageProps}
+          hasClickedGetStarted={hasClickedGetStarted}
+          onGetStarted={handleGetStarted}
+          keynames={keynames}
+          fileObject={fileObject}
+          onUploadFile={handleUploadFile}
+          onConversion={handleConversion}
+          clickedChartType={clickedChartType}
+          onSelectChartType={handleSelectChartType}
+          xKey={xKey}
+          yKey={yKey}
+          xVariable={xVariable}
+          yVariable={yVariable}
+          onXChange={handleXChange}
+          onYChange={handleYChange}
+          onAssignVariables={handleAssignVariables}
+          settings={settings}
+          onSettingsChange={handleSettingsChange}
+          hasCompletedStep4={hasCompletedStep4}
+          onHasCompletedStep4={handleHasCompletedStep4}
+          hasCompletedStep5={hasCompletedStep5}
+          onHasCompletedStep5={handleHasCompletedStep5}
+          hasCompletedDistributionProperties={
+            hasCompletedDistributionProperties
+          }
+          onHasCompletedDistributionProperties={
+            handleHasCompletedDistributionProperties
+          }
+          hasCompletedStep6={hasCompletedStep6}
+          onHasCompletedStep6={handleHasCompletedStep6}
+        />
+      </Layout>
+    </SessionProvider>
   );
 }
