b038c3dd6d452d54359e70e2ae409c6f6fdc89d5
Maria Adner <juergens.maria@googlemail.com>
2024-05-06 10:04:17 +0200

    updated schema

--- db/models/Plot.js
+++ db/models/Plot.js
@@ -2,15 +2,12 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const plotSchema = new Schema(
-  {
-    clickedChartType: { type: String, required: true },
-    xVariable: { type: Array, required: true },
-    yVariable: { type: Array, required: true },
-    settings: { type: Object, required: true },
-  },
-  { collection: "plot-settings" }
-);
+const plotSchema = new Schema({
+  clickedChartType: { type: String, required: true },
+  xVariable: { type: Array, required: true },
+  yVariable: { type: Array, required: true },
+  settings: { type: Object, required: true },
+});
 
 const Plot = mongoose.models.Plot || mongoose.model("Plot", plotSchema);
 
