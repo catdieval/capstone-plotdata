918e9247dd42396525eeb227d69c5bfe9a473cef
Maria Adner <juergens.maria@googlemail.com>
2024-05-06 09:30:32 +0200

    updated SignInForm component

--- Components/SignInForm/index.js
+++ Components/SignInForm/index.js
@@ -4,5 +4,19 @@ import Button from "../Button";
 export default function SingInForm() {
   const { data: session } = useSession();
 
-  return <Button></Button>;
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email}
+        <Button onClick={() => signOut()}>Sign out</Button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        Not signed in
+        <Button onClick={() => signIn()}>Sign in</Button>
+      </>
+    );
+  }
 }
