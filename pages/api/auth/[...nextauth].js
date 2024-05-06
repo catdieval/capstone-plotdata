d420614113887ee060298db99b4d8772dd5a9128
Maria Adner <juergens.maria@googlemail.com>
2024-05-06 23:29:35 +0200

    updated api nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,28 +1,41 @@
 import NextAuth from "next-auth";
-
 import GithubProvider from "next-auth/providers/github";
-
+import CredentialsProvider from "next-auth/providers/credentials";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-
 import clientPromise from "../../../db/mongodb";
-
 import dbConnect from "../../../db/connect";
+import User from "../../../db/models/User";
 
-import User from "../../..//db/models/User";
-
-export default NextAuth({
-  // Configure one or more authentication providers
-
+export const authOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
-
       clientSecret: process.env.GITHUB_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username:", type: "text" },
+        password: {
+          label: "Password:",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (
+          credentials.name === "plotdata" &&
+          credentials.password === "plotdata"
+        ) {
+          return { name: "plotdata", email: "plotdata@gmail.com" };
+        } else {
+          return null;
+        }
+      },
     }),
   ],
 
   adapter: MongoDBAdapter(clientPromise),
-
+  session: { strategy: "jwt" },
   callbacks: {
     async session({ session, user }) {
       dbConnect();
@@ -38,4 +51,6 @@ export default NextAuth({
       return { ...session, user: { ...session.user, id: user.id } };
     },
   },
-});
+};
+
+export default NextAuth(authOptions);
