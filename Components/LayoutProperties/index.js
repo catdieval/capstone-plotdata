51816c2fee6b1a099b164a926599f83ae6ae7f57
Catherine <catherine.dieval@hotmail.fr>
2024-10-17 17:11:37 +0200

    Rationalised presence/absence of form element and InputTypeSubmit for these components

--- Components/LayoutProperties/index.js
+++ Components/LayoutProperties/index.js
@@ -3,26 +3,16 @@ import { StyledH3 } from "../Heading";
 import LogScaleProperties from "../LogScaleProperties";
 import RangeProperties from "../RangeProperties";
 import Container from "../Container";
-/*import InputTypeSubmit from "../InputTypeSubmit";
-import {ButtonContainer} from "../Navigation/styledNavigation";
-import Button from "../Button"; */
 
 export default function LayoutProperties({ 
-  /*onDisableNextButton, 
-  onNext, 
-  onBack, */
   settings, 
   onSettingsChange, 
 }) {
-  /*function handleSubmit(event) {
-    event.preventDefault();
-  } */
 
   return (
     <>
       <Container $centered="center" $margin>
         <StyledH3>Layout</StyledH3>
-        
           <GridProperties
             settings={settings}
             onSettingsChange={onSettingsChange}
@@ -35,8 +25,6 @@ export default function LayoutProperties({
             settings={settings}
             onSettingsChange={onSettingsChange}
           />
-          
-       
       </Container>
     </>
   );
